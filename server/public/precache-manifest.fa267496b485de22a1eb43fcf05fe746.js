8521065b9945003cb18e7cad27b81eaf70136f1a
laanayam333 <laanayam333@gmail.com>
2019-08-28 17:01:45 +0200

    deploy2

---
+++ server/public/precache-manifest.fa267496b485de22a1eb43fcf05fe746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5266d231a87eb4e16dbb36383819ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed7560b1e15b2f44a19e",
+    "url": "/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "bb37dd5f3bed6e5fb3f3",
+    "url": "/static/css/main.f9ea59c0.chunk.css"
+  },
+  {
+    "revision": "ed7560b1e15b2f44a19e",
+    "url": "/static/js/2.771ab010.chunk.js"
+  },
+  {
+    "revision": "bb37dd5f3bed6e5fb3f3",
+    "url": "/static/js/main.4d1bab44.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
